f0336f1c3f87afbf5682e5ad66953b422eda7122
Ivan Putra Eriansya <eriansha.van@gmail.com>
2023-10-22 22:53:57 +0700

    feat: add TextFields

--- DesignGuideline/ContentView.swift
+++ DesignGuideline/ContentView.swift
@@ -11,6 +11,8 @@ struct ContentView: View {
     var body: some View {
         VStack {
             Buttons()
+            
+            TextFields()
         }
         .padding()
     }
---
+++ DesignGuideline/Views/TextFields.swift
@@ -0,0 +1,25 @@
+//
+//  TextFields.swift
+//  DesignGuideline
+//
+//  Created by Ivan on 22/10/23.
+//
+
+import SwiftUI
+
+struct TextFields: View {
+    var body: some View {
+        VStack {
+            HStack {
+                Text("Text Field")
+                    .font(.largeTitle)
+                    .fontWeight(.bold)
+                Spacer()
+            }
+        }
+    }
+}
+
+#Preview {
+    TextFields()
+}
