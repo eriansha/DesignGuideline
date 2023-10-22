2b997b22b9625cef37cc87fac81c68cf2181a812
Ivan Putra Eriansya <eriansha.van@gmail.com>
2023-10-22 22:01:57 +0700

    Initial Commit

---
+++ DesignGuideline/DesignGuidelineApp.swift
@@ -0,0 +1,17 @@
+//
+//  DesignGuidelineApp.swift
+//  DesignGuideline
+//
+//  Created by Ivan on 22/10/23.
+//
+
+import SwiftUI
+
+@main
+struct DesignGuidelineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
